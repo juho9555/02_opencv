cc7871a62791e9cf587f4a27d7aaba3d51e0289d
JUHO <ijuho9555@gmail.com>
2025-07-29 15:41:21 +0900

    히스토그램으로 rgb값을 그래프화

---
+++ src/hist_rgb.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+import matplotlib.pylab as plt
+
+# 이미지 그레이 스케일로 읽기 및 출력
+img = cv2.imread('../img/like_lenna.png')
+cv2.imshow('img', img)
+
+# 히스토그램 계산 및 그리기
+#hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+
+channels = cv2.split(img)
+colors = ('b', 'g', 'r')
+for (ch, color) in zip (channels, colors):
+    hist = cv2.calcHist([ch], [0], None, [256], [0, 256])
+    plt.plot(hist, color = color)
+plt.show()
+#plt.plot(hist)
+
+#print("hist.shape", hist.shape) # 히스토그램의 shape(256, 1)
+#print("hist.sum():", hist.sum(), "img.shape:", img.shape) # 히스토그램 총 합계와 이미지의 크기
+#plt.show()
