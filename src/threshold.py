1e01a03ba969ecc996a7e88b320e689fe0182d19
JUHO <ijuho9555@gmail.com>
2025-07-29 11:19:21 +0900

    오츠의 알고리즘을 통해 최적의 임계값 찾기 코드

--- src/threshold.py
+++ src/threshold.py
@@ -1,20 +1,16 @@
-# 전역 스레시홀딩 (threshold.py)
 import cv2
 import numpy as np
 import matplotlib.pylab as plt
 
-img = cv2.imread('../img/like_lenna.png', cv2.IMREAD_GRAYSCALE) #이미지를 그레이 스케일로 읽기
+# 이미지를 그레이 스케일로 읽기
+img = cv2.imread('../img/like_lenna.png', cv2.IMREAD_GRAYSCALE) 
+# 경계 값을 130으로 지정  ---①
+_, t_140 = cv2.threshold(img, 140, 255, cv2.THRESH_BINARY)        
+# 경계 값을 지정하지 않고 OTSU 알고리즘 선택 ---②
+t, t_otsu = cv2.threshold(img, -1, 255,  cv2.THRESH_BINARY | cv2.THRESH_OTSU) 
+print('otsu threshold:', t)                 # Otsu 알고리즘으로 선택된 경계 값 출력
 
-# --- ① NumPy API로 바이너리 이미지 만들기
-thresh_np = np.zeros_like(img)   # 원본과 동일한 크기의 0으로 채워진 이미지
-thresh_np[img > 127] = 255      # 127 보다 큰 값만 255로 변경
-
-# ---② OpenCV API로 바이너리 이미지 만들기
-ret, thresh_cv = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY) 
-print(ret)  # 127.0, 바이너리 이미지에 사용된 문턱 값 반환
-
-# ---③ 원본과 결과물을 matplotlib으로 출력
-imgs = {'Original': img, 'NumPy API':thresh_np, 'cv2.threshold': thresh_cv}
+imgs = {'Original': img, 't:140':t_140, 'otsu:%d'%t: t_otsu}
 for i , (key, value) in enumerate(imgs.items()):
     plt.subplot(1, 3, i+1)
     plt.title(key)
